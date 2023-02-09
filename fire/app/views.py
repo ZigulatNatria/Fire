153a1c96efb2be525a6b6d8d2593970662c870a1
OVahrameev <vachrameev.oleg@yandex.ru>
2023-02-09 17:56:01 +0500

    Добавлен цикл в html во вьюхе надо перебрать прогресс

--- fire/app/views.py
+++ fire/app/views.py
@@ -4,9 +4,15 @@ from .models import *
 # Create your views here.
 def sports(request, sport_id):
     sport = Sport.objects.get(pk=sport_id)
-    discipline = Discipline.objects.get(sport=sport_id)
+    discipline = Discipline.objects.filter(sport=sport_id)
     # discipline_id = discipline.objects.get()
-    progress = Progress.objects.filter(discipline_progress=discipline.pk)
+    # progress = Progress.objects.filter(discipline_progress=discipline.pk)
+
+   #TODO получить все поля модели
+    progress = []
+    for d in discipline:
+        p = d.id
+        progress.append(Progress.objects.filter(discipline_progress=p))
     trainer = Trainer.objects.filter(sport=sport)
 
     context = {'sport': sport,
@@ -14,5 +20,5 @@ def sports(request, sport_id):
                'progress': progress,
                'trainer': trainer,
                }
-    # return render(request, 'test.html', context)
-    return render(request, 'freestyle.html', context)
+    return render(request, 'test.html', context)
+    # return render(request, 'freestyle.html', context)
