cae389ae5baca8328143bec86d14d7ad205b0155
ZigulatNatria <96946885+ZigulatNatria@users.noreply.github.com>
2023-02-11 01:41:56 +0500

    стоит попробовать многое ко многим......

--- fire/app/models.py
+++ fire/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Sport(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,14 +16,31 @@ class Discipline(models.Model):
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image_1 = models.ImageField()
-    image_2 = models.ImageField()
+    image_1 = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'Дисциплина'
+
+
+class Progress(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    athlete = models.CharField(max_length=100, null=True, blank=True)
+    progress = models.ForeignKey(Discipline, on_delete=models.CASCADE, null=True, blank=True)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.athlete
+
+    class Meta:
+        verbose_name = 'Достижения'
+
+
 
 
 # class Progress(models.Model):
@@ -38,17 +55,7 @@ class Discipline(models.Model):
 #     class Meta:
 #         verbose_name = 'Достижения'
 
-class Progress(models.Model):
-    discipline_progress = models.ForeignKey(Discipline, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    athlete = models.CharField(max_length=100, null=True, blank=True)
 
-    def __str__(self):
-        return self.athlete
-
-    class Meta:
-        verbose_name = 'Достижения'
 
 
 class Trainer(models.Model):
--- fire/app/views.py
+++ fire/app/views.py
@@ -3,20 +3,17 @@ from .models import *
 
 # Create your views here.
 def sports(request, sport_id):
+    contacts = Contacts.objects.all()
     sport = Sport.objects.get(pk=sport_id)
-    discipline = Discipline.objects.filter(sport=sport_id)
-
-# Циклом перебераем все сущности Progress по id Discipline
-    for d in discipline:
-        p = d.id
-        progress = Progress.objects.filter(discipline_progress=p)
     trainer = Trainer.objects.filter(sport=sport)
+    discipline = Discipline.objects.filter(sport=sport_id)
+    progress = Progress.objects.filter(sport=sport_id)
 
     context = {'sport': sport,
                'discipline': discipline,
-               'progress': progress,
                'trainer': trainer,
-               'p': p
+               'contacts': contacts,
+               'progress': progress
                }
     return render(request, 'test.html', context)
     # return render(request, 'freestyle.html', context)
