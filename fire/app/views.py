8d856b649c8c04f62f9bc3ed47392fb1281d419f
OVahrameev <vachrameev.oleg@yandex.ru>
2023-02-10 17:56:36 +0500

    Настроен во view доступ к сущностяп прогресса через id дисциплины

--- fire/app/views.py
+++ fire/app/views.py
@@ -5,20 +5,18 @@ from .models import *
 def sports(request, sport_id):
     sport = Sport.objects.get(pk=sport_id)
     discipline = Discipline.objects.filter(sport=sport_id)
-    # discipline_id = discipline.objects.get()
-    # progress = Progress.objects.filter(discipline_progress=discipline.pk)
 
-   #TODO получить все поля модели
-    progress = []
+# Циклом перебераем все сущности Progress по id Discipline
     for d in discipline:
         p = d.id
-        progress.append(Progress.objects.filter(discipline_progress=p))
+        progress = Progress.objects.filter(discipline_progress=p)
     trainer = Trainer.objects.filter(sport=sport)
 
     context = {'sport': sport,
                'discipline': discipline,
                'progress': progress,
                'trainer': trainer,
+               'p': p
                }
     return render(request, 'test.html', context)
     # return render(request, 'freestyle.html', context)
