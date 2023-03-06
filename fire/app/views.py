3db93f2348fa4fb8713b2caeb7ee0c3a0cee4b65
ZigulatNatria <96946885+ZigulatNatria@users.noreply.github.com>
2023-03-06 23:30:19 +0500

    Перебрать HTML заново

--- fire/app/views.py
+++ fire/app/views.py
@@ -47,7 +47,8 @@ def sports(request, sport_id):
                }
     # return render(request, 'test.html', context)
     # return render(request, 'test3.html', context)
-    return render(request, 'freestyle.html', context)
+    # return render(request, 'freestyle.html', context)
+    return render(request, 'freestyle_good.html', context)
 
 
 class SportListView(ListView):
